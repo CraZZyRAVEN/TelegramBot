4fb93cea73e45aba66d98ad511c3f628dddb0c6f
crazzyraven <karas1377@gmail.com>
2023-05-06 09:21:53 +0300

    added count_symbols func

--- Main.py
+++ Main.py
@@ -1,8 +1,13 @@
 from aiogram import Dispatcher, executor, types, Bot
-import token_api
+from token_api import TOKEN_API
 
-bot = bot(token=token_api)
+bot = Bot(token=TOKEN_API)
 dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler()
+async def count_symbols(msg: types.Message) -> None:
+    await msg.answer(len(msg.text) - msg.text.count(' '))
 
 
 if __name__ == '__main__':

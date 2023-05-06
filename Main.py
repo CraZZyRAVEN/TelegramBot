5ad6e3a3c1e07b06e9be564202b41aef959b6199
crazzyraven <karas1377@gmail.com>
2023-05-06 09:05:19 +0300

    init

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 from aiogram import Dispatcher, executor, types, Bot
+import token_api
 
-TOKEN_API = '6016467207:AAE4WULwZnLPIM91nctrTyR5o5kznh4EXrA'
-
-bot = bot(token=TOKEN_API)
+bot = bot(token=token_api)
 dp = Dispatcher(bot=bot)
+
 
 if __name__ == '__main__':
     executor.start_polling(dispatcher=dp)
